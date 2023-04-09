e2db074ff035907b0bcf6558782eea3d53b6ba3f
faye <faye_1225@163.com>
2023-04-10 02:50:08 +0800

    fix:link #1

--- install.py
+++ install.py
@@ -1,6 +1,8 @@
 import launch
 
-launch.run_pip("install huggingface-hub==0.11.0", "requirements for WebUI Share")
+if not launch.is_installed("huggingface_hub"):
+    launch.run_pip("install huggingface-hub==0.13.4", "requirements for WebUI Share")
+
 
 if not launch.is_installed("send2trash"):
     launch.run_pip("install Send2Trash", "requirement for WebUI Share")
